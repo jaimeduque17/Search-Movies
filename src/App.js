43419fa821070d8781bb984ae956b15814ce8d8e
Jaime Duque <jaimeduque17@gmail.com>
2019-09-14 18:17:16 -0500

    React-router-dom implementated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
@@ -8,15 +9,12 @@ import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
-    const url = new URL(document.location)
-    const Page = url.searchParams.has('id')
-    ? <Detail id={url.searchParams.get('id')} /> 
-  : <Home /> 
-
-
     return (
       <div className="App" >
-        {Page}
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/detail/:movieId' component={Detail} />
+        </Switch>
       </div>
     );
   }
