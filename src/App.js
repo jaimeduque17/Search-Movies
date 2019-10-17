3ecbba3ac40b5076809d5504b544471cac00c16f
Jaime Duque <jaimeduque17@gmail.com>
2019-10-16 21:44:49 -0500

    Some styles are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
 import { NotFound } from './pages/NotFound';
 
 import './App.css';
-import 'bulma/css/bulma.css'
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   render() {
